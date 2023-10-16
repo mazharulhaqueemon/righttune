a919e176ba4b023471289c02a6df973d7023623a
mazharulhaqueemon <mazharulhaqueemon@gmail.com>
2023-10-16 16:00:54 +0600

    post urls and all fixed

--- fcm/api/views.py
+++ fcm/api/views.py
@@ -19,8 +19,10 @@ from agora_token_builder import RtcTokenBuilder
 
 # APP_ID = '10efeca3cee04cd5bdad0c2bdc09f119';
 # APP_CERTIFICATE = '76f664ccd9254775b06becb9c94d8d70';
-APP_ID = 'fb1751907aba4dc293a4848c708f4749';
-APP_CERTIFICATE = 'fb1751907aba4dc293a4848c708f4749';
+APP_ID = 'fb1751907aba4dc293a4848c708f4749'
+APP_CERTIFICATE = '55086a987da64194ba54def7b0d3b9f3'
+
+
 class AgoraRtcTokenRetrieveApiView(RetrieveAPIView):
     authentication_classes = []
     permission_classes = []
